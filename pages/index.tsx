4325d7aa7a0ac9afa420aa698d9ebeb169e9a3d1
Ansheel9 <banthiaansheel0912@gmail.com>
2022-04-17 22:24:36 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,67 +14,67 @@ import ScrollableBlogPosts from 'views/HomePage/ScrollableBlogPosts';
 import Testimonials from 'views/HomePage/Testimonials';
 
 export default function Homepage({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return (
-    <>
-      <Head>
-        <title>{EnvVars.SITE_NAME}</title>
-        <meta
-          name="description"
-          content="Tempor nostrud velit fugiat nostrud duis incididunt Lorem deserunt est tempor aute dolor ad elit."
-        />
-      </Head>
-      <HomepageWrapper>
-        <WhiteBackgroundContainer>
-          <Hero />
-          {/* <Partners /> */}
-            <BasicSection imageUrl="/test1.png" title="INVESTING IS BETTER WITH FRIENDS" overTitle="Discuss">
-            <h5>Investing Gone Social</h5>
-            <p>
-             Create private chats with your gang, see what your friends are investing in, get instant data-rich insights as you discuss and share ideas with each other.
-             </br> Psst...We do not disclose your Investment Amount to anyone.
-
-            </p>
-          </BasicSection>
-
-          <BasicSection imageUrl="/test1.png" title="CREATE, COPY AND SHARE YOUR STOCK PLAYLISTS" overTitle="COLLABORATE" reversed>
-            <p>
-            Create your own playlist of stocks, make one with your group or just find investors you believe in. Reduce risk, invest across different companies so you're not putting all your eggs in one basket, literally.
-
-            </p>            
-            </BasicSection>
-
-            <BasicSection imageUrl="/test1.png" title="INVEST IN WHAT YOU BELIEVE IN, WITH AS LOW AS ₹50" overTitle="INVEST">
-            <p>
-            Invest in US Companies, Indian Companies, ETFs and Mutual Funds. </br>
-            If you believe Tesla holds the future for cars, be a part of it for as low as ₹50.
-            </p>
-          </BasicSection>
-
-          <BasicSection imageUrl="/test1.png" title="WIN UPTO ₹1 LAKH EVERY WEEK" overTitle="WIN" reversed>
-            <p>
-            Earn Tokens with every investment you make, ₹1 = 1 token. </br> Participate with the tokens in weekly games or win by showing off your group’s investing power on the weekly leaderboard.
-
-            </p>
-            
-          </BasicSection>
-
-          <BasicSection imageUrl="/test1.png" title="(L)EARN MONEY" overTitle="EARN">
-            <p>
-            Don't worry, we do the hard work. </br> 
-            It’s hard to get time from your busy schedule. We get it. Get insights in a bite-sized form. Stay on top with the latest news. Discuss it with friends. All at one place.
-            </p>
-          </BasicSection>
-        </WhiteBackgroundContainer>
-        <DarkerBackgroundContainer>
-          <Cta />
-          <FeaturesGallery />
-          <Features />
-          <Testimonials />
-          <ScrollableBlogPosts posts={posts} />
-        </DarkerBackgroundContainer>
-      </HomepageWrapper>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>{EnvVars.SITE_NAME}</title>
+                <meta
+                    name="description"
+                    content="Tempor nostrud velit fugiat nostrud duis incididunt Lorem deserunt est tempor aute dolor ad elit."
+                />
+            </Head>
+            <HomepageWrapper>
+                <WhiteBackgroundContainer>
+                    <Hero />
+                    <Partners />
+                    <BasicSection imageUrl="/demo-test1.png" title="INVESTING IS BETTER WITH FRIENDS" overTitle="Discuss">
+                        <h5>Investing Gone Social</h5>
+                        <p>
+                        Create private chats with your gang, see what your friends are investing in, get instant data-rich insights as you discuss and share ideas with each other.
+                        <br>Psst...We do not disclose your Investment Amount to anyone.
+                        </p>
+                    </BasicSection>
+                    <BasicSection imageUrl="/demo-test1.png" title="CREATE, COPY AND SHARE YOUR STOCK PLAYLISTS" overTitle="Collaborate" reversed>
+                        <h5>More Companies, Less Risk</h5>
+                        <p>
+                        Create your own playlist of stocks, make one with your group or just find investors you believe in. Reduce risk, invest across different companies so you're not putting all your eggs in one basket, literally.
+                        </p>
+                    </BasicSection>
+                        <BasicSection imageUrl="/demo-test1.png" title="INVEST IN WHAT YOU BELIEVE IN, WITH AS LOW AS ₹50" overTitle="Invest">
+                        <h5>Invest in US Companies, Indian Companies, ETFs and Mutual Funds.</h5>
+                        <p>
+                        Now own a Tesla, Apple or Netflix Stock with as low as ₹50 or invest in that hot IPO you have been waiting for.
+                        </p>
+                    </BasicSection>
+                    <BasicSection imageUrl="/demo-test1.png" title="WIN UPTO ₹1 LAKH EVERY WEEK" overTitle="Win" reversed>
+                        <p>
+                            <h5> Earn tokens with every investment you make, ₹1 = 1 token. </h5> 
+                            </br> Participate with the tokens in weekly games or win by showing off your group's investing power on the weekly leaderboard.
+                        </p>
+                        <ul>
+                            <li>Professional point 1</li>
+                            <li>Professional remark 2</li>
+                            <li>Professional feature 3</li>
+                        </ul>
+                    </BasicSection>
+                    <BasicSection imageUrl="/demo-test1.png" title="(L)EARN MONEY" overTitle="Earn">
+                        <p>
+                        </br> Don't worry, we do the hard work. </br> 
+                        It’s hard to get time from your busy schedule. We get it. Get insights in a bite-sized form. Stay on top with the latest news. Discuss it with friends. All at one place.
+                        </p>
+                    </BasicSection>
+                    
+                </WhiteBackgroundContainer>
+                <DarkerBackgroundContainer>
+                    <Cta />
+                    <FeaturesGallery />
+                    <Features />
+                    <Testimonials />
+                    <ScrollableBlogPosts posts={posts} />
+                </DarkerBackgroundContainer>
+            </HomepageWrapper>
+        </>
+    );
 }
 
 const HomepageWrapper = styled.div`
