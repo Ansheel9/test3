77b345d67488276ee40195f5c48b33f1add62d87
Ansheel9 <banthiaansheel0912@gmail.com>
2022-04-17 22:41:59 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import { InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import styled from 'styled-components';
 import BasicSection from 'components/BasicSection';
-import Link from 'components/Link';
 import { EnvVars } from 'env';
 import { getAllPosts } from 'utils/postsFetcher';
 import Cta from 'views/HomePage/Cta';
@@ -28,35 +27,35 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
                     <Hero />
                     <Partners />
                     <BasicSection imageUrl="/demo-test1.png" title="INVESTING IS BETTER WITH FRIENDS" overTitle="Discuss">
-                        <h5>Investing Gone Social</h5>
-                        <p>
+                      <p>
+                        Investing Gone Social
                         Create private chats with your gang, see what your friends are investing in, get instant data-rich insights as you discuss and share ideas with each other.{"\n"}
                         Psst...We do not disclose your Investment Amount to anyone.
-                        </p>
+                      </p>
                     </BasicSection>
                     <BasicSection imageUrl="/demo-test1.png" title="CREATE, COPY AND SHARE YOUR STOCK PLAYLISTS" overTitle="Collaborate" reversed>
-                        <h5>More Companies, Less Risk</h5>
-                        <p>
+                      <p>
+                        More Companies, Less Risk
                         Create your own playlist of stocks, make one with your group or just find investors you believe in. Reduce risk, invest across different companies so you're not putting all your eggs in one basket, literally.
-                        </p>
+                      </p>
                     </BasicSection>
                         <BasicSection imageUrl="/demo-test1.png" title="INVEST IN WHAT YOU BELIEVE IN, WITH AS LOW AS ₹50" overTitle="Invest">
-                        <h5>Invest in US Companies, Indian Companies, ETFs and Mutual Funds.</h5>
-                        <p>
+                      <p>
+                        Invest in US Companies, Indian Companies, ETFs and Mutual Funds.
                         Now own a Tesla, Apple or Netflix Stock with as low as ₹50 or invest in that hot IPO you have been waiting for.
-                        </p>
+                      </p>
                     </BasicSection>
                     <BasicSection imageUrl="/demo-test1.png" title="WIN UPTO ₹1 LAKH EVERY WEEK" overTitle="Win" reversed>
-                        <h5> Earn tokens with every investment you make, ₹1 = 1 token. </h5> 
-                        <p>
+                      <p>
+                        Earn tokens with every investment you make, ₹1 = 1 token.
                         Participate with the tokens in weekly games or win by showing off your group's investing power on the weekly leaderboard.
-                        </p>  
+                      </p>  
                     </BasicSection>
                     <BasicSection imageUrl="/demo-test1.png" title="(L)EARN MONEY" overTitle="Earn">
-                        <h5>Don't worry, we do the hard work. </h5> 
-                        <p>
+                      <p>  
+                        Don't worry, we do the hard work.
                         It’s hard to get time from your busy schedule. We get it. Get insights in a bite-sized form. Stay on top with the latest news. Discuss it with friends. All at one place.
-                        </p>
+                      </p>
                     </BasicSection>
                     
                 </WhiteBackgroundContainer>
