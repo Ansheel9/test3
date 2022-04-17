d63ca63b6db159318eba37b6e2c902e1cb9dac30
Ansheel9 <banthiaansheel0912@gmail.com>
2022-04-17 19:34:28 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,26 +47,26 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
               <li>Professional feature 3</li>
             </ul>
             </BasicSection>
-                  <BasicSection imageUrl="/test1.png" title="Lorem ipsum dolor sit amet consectetur." overTitle="sit amet gogo">
-                      <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore
-                          voluptate quo deleniti animi laboriosam.{' '}
-                          <Link href="/help-center">Possimus ullam velit rem itaque consectetur, in distinctio?</Link> Lorem ipsum, dolor sit amet
-                          consectetur adipisicing elit. Soluta repellendus quia quos obcaecati nihil. Laudantium non accusantium, voluptate eum nesciunt
-                          at suscipit quis est soluta?
-                      </p>
-                  </BasicSection>
-                  <BasicSection imageUrl="/demo-illustration-2.svg" title="Lorem ipsum dolor sit." overTitle="lorem ipsum" reversed>
-                      <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore{' '}
-                          <strong>voluptate quo deleniti animi laboriosam</strong>. Possimus ullam velit rem itaque consectetur, in distinctio?
-                      </p>
-                      <ul>
-                          <li>Professional point 1</li>
-                          <li>Professional remark 2</li>
-                          <li>Professional feature 3</li>
-                      </ul>
-                  </BasicSection>
+            <BasicSection imageUrl="/test1.png" title="Lorem ipsum dolor sit amet consectetur." overTitle="sit amet gogo">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore
+              voluptate quo deleniti animi laboriosam.{' '}
+              <Link href="/help-center">Possimus ullam velit rem itaque consectetur, in distinctio?</Link> Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Soluta repellendus quia quos obcaecati nihil. Laudantium non accusantium, voluptate eum nesciunt
+              at suscipit quis est soluta?
+            </p>
+          </BasicSection>
+          <BasicSection imageUrl="/demo-illustration-2.svg" title="Lorem ipsum dolor sit." overTitle="lorem ipsum" reversed>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore{' '}
+              <strong>voluptate quo deleniti animi laboriosam</strong>. Possimus ullam velit rem itaque consectetur, in distinctio?
+            </p>
+            <ul>
+              <li>Professional point 1</li>
+              <li>Professional remark 2</li>
+              <li>Professional feature 3</li>
+            </ul>
+          </BasicSection>
         </WhiteBackgroundContainer>
         <DarkerBackgroundContainer>
           <Cta />
